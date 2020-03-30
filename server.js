050ef0ee5add4e1aaeb2da31842c0ebf9d8c035e
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2020-03-30 21:44:25 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate with CodePipeline!V2');
+  response.end('Hello from Node Server App deployedzzzzzzzzzzzzzzzzate with CodePipeline!V2');
   
 };
 var www = http.createServer(handleRequest);
